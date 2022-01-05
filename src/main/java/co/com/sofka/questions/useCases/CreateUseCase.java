e0fb6c63ac55f7ff52d1f8782057b4f2c78a2d59
Juan Pablo Cano <Canojuan0415@gmail.com>
2022-01-05 15:32:03 -0500

    Creación de casos de uso y routers

---
+++ src/main/java/co/com/sofka/questions/useCases/CreateUseCase.java
@@ -0,0 +1,26 @@
+package co.com.sofka.questions.useCases;
+
+import co.com.sofka.questions.collections.Question;
+import co.com.sofka.questions.model.QuestionDTO;
+import co.com.sofka.questions.repositories.QuestionRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
+import reactor.core.publisher.Mono;
+
+@AllArgsConstructor
+@Service
+@Validated
+public class CreateUseCase implements SaveQuestion {
+    private final QuestionRepository questionRepository;
+    private final MapperUtils mapperUtils;
+
+
+    @Override
+    public Mono<String> apply(QuestionDTO newQuestion) {
+        return questionRepository
+                .save(mapperUtils.mapperToQuestion(null).apply(newQuestion))
+                .map(Question::getId);
+    }
+
+}
---
+++ src/main/java/co/com/sofka/questions/useCases/ListUseCase.java
@@ -0,0 +1,24 @@
+package co.com.sofka.questions.useCases;
+
+import co.com.sofka.questions.model.QuestionDTO;
+import co.com.sofka.questions.repositories.QuestionRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
+import reactor.core.publisher.Flux;
+
+import java.util.function.Supplier;
+
+@AllArgsConstructor
+@Service
+@Validated
+public class ListUseCase implements Supplier<Flux<QuestionDTO>> {
+    private final QuestionRepository questionRepository;
+    private final MapperUtils mapperUtils;
+
+    @Override
+    public Flux<QuestionDTO> get() {
+        return questionRepository.findAll()
+                .map(mapperUtils.mapEntityToQuestion());
+    }
+}
